1e07fd7112d665e05934ca4ecd99f63c86da3a99
Ryan Abbott <rabbott2937@gmail.com>
2014-07-22 19:19:50 -0400

    Reworked function
    Changed number of nodes into an argument

--- SymmetryChecker/main.cpp
+++ SymmetryChecker/main.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 using namespace std;
 
-#define NODES 16
-
 int optimal8node[16][16] = { { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
 							 { 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
 							 { 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0 },
@@ -21,32 +19,31 @@ int optimal8node[16][16] = { { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
 							 { 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1 },
 							 { 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0 } };
 
-int checkSymmetry(int * graph);
+int checkSymmetry(int * graph, int nodes);
 
 int main(){
-	cout << "CheckSymmetry(8 node optimal) = " << checkSymmetry(&optimal8node[0][0]) << "\n";
+	cout << "CheckSymmetry(8 node optimal) = " << checkSymmetry(&optimal8node[0][0], 16) << "\n";
 	getchar();
 	return 0;
 }
 
-int checkSymmetry(int * graph){
+int checkSymmetry(int * graph, int nodes){
 	int counter = 0;
 	vector<int> neighbors;
 	int symmetries = 0;
 	bool nonSymmetryFound = false;
-	for (int x = 0; x < NODES / 2; x++){
-		for (int i = x; i < NODES / 2 + x; i++){
+	for (int x = 0; x < nodes / 2; x++){
+		for (int i = x; i < nodes / 2 + x; i++){
 			neighbors.clear();
-			for (int n = 0; n < NODES; n++){
-				if (graph[NODES * i + n]) neighbors.push_back(n);
+			for (int n = 0; n < nodes; n++){
+				if (graph[nodes * i + n]) neighbors.push_back(n);
 			}
 			//v is the node opposite to
-			int v = (NODES/2 + x + (NODES/2 + x - i) - 1) % NODES;
+			int v = (nodes/2 + x + (nodes/2 + x - i) - 1) % nodes;
 			nonSymmetryFound = false;
 			for (int j = 0; j < 3; j++){
-				int u = (NODES / 2 + x + (NODES / 2 + x - neighbors.at(j)) - 1) % NODES;
-				if (graph[u + v * NODES] != 1){
-					counter++;
+				int u = (nodes / 2 + x + (nodes / 2 + x - neighbors.at(j)) - 1) % nodes;
+				if (graph[u + v * nodes] != 1){
 					nonSymmetryFound = true;
 					break; 
 				}
@@ -55,6 +52,5 @@ int checkSymmetry(int * graph){
 		}
 		if(nonSymmetryFound == false) symmetries++;
 	}
-	cout << "Counter = " << counter << "\n";
 	return symmetries;
 }
